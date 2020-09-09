6fdb2b4aa57a20991b990dd9384bd7c94abfaaaf
unknown <urvesharpita@gmail.com>
2020-09-09 16:37:13 +0100

    My First Commit

---
+++ CoreMVCBasicCURD/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVCBasicCURD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required(ErrorMessage ="please enter employee full name..")]
+        [DisplayName("Full Name")]
+        public string FulllNmae { get; set; }
+
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("Employee UserName")]
+        public string EmpCode { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+
+    }
+}
