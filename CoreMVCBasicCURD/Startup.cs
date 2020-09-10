3915efddfea0468ed46609c3b94165d8339f5a3d
unknown <urvesharpita@gmail.com>
2020-09-10 18:45:27 +0100

    Added web API for ASP.NET Core app, Added Swagger testing tool.

--- CoreMVCBasicCURD/Startup.cs
+++ CoreMVCBasicCURD/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CoreMVCBasicCURD.Models;
+using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -24,11 +25,21 @@ namespace CoreMVCBasicCURD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Telling  .NET Core look for following services  e.g Controller, View and Models...may be Web API
             services.AddControllersWithViews();
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
-
+            
+            //Create an Instance of DbContext that interact with the database
             services.AddDbContext<EmployeeContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("TestEmployeeConnection")));
+
+            services.AddSwaggerGen(c => 
+            {
+               // c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo  { Title ="Employee API", Version="v1" });
+               
+            }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +59,15 @@ namespace CoreMVCBasicCURD
 
             app.UseAuthorization();
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee Api");              
+            });
+
+            // using declared services in ConfigureServices
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
